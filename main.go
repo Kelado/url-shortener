28cfc8748cdf37b000e4bd1177ec439303dbc7b8
Kelado <kelantonag@gmail.com>
2024-09-25 03:48:53 +0300

    Connect MVC in main

--- main.go
+++ main.go
@@ -3,22 +3,31 @@ package main
 import (
 	"net/http"
 
+	controller "github.com/Kelado/url-shortener/controllers"
+	handler "github.com/Kelado/url-shortener/handlers"
+	"github.com/Kelado/url-shortener/repositories"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 const (
-	Addr = ":8000"
+	Addr     = ":8000"
+	CodeSize = 6
+	Hostname = "http://localhost:8000/"
 )
 
 func main() {
+	linkRepo := repositories.NewMockDB()
+	controller := controller.NewController(Hostname, CodeSize, linkRepo)
+	handler := handler.NewHandler(controller)
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	r.Get("/ping", HandlePing)
+	r.Get("/ping", handler.HandlePing)
 
-	r.Post("/shorten", HandlePostURL)
-	r.Get("/:shortUrl", HandleGetURL)
+	r.Post("/shorten", handler.HandlePostURL)
+	r.Get("/:shortUrl", handler.HandleGetURL)
 
 	http.ListenAndServe(Addr, r)
 }
