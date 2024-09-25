303da702bc75a46f821008b28a1c3ef2eee66146
Kelado <kelantonag@gmail.com>
2024-09-25 22:57:57 +0300

    Added Test cases: catch error case from controller module

--- controllers/controller_test.go
+++ controllers/controller_test.go
@@ -46,3 +46,46 @@ func TestSameGeneratedCode(t *testing.T) {
 	codeGenerated := string(linkResp)[len(linkResp)-len(finalCode):]
 	assert.Equal(t, finalCode, codeGenerated)
 }
+
+func TestCreateWithEmptyURL(t *testing.T) {
+	c := newController()
+	url := models.URL("")
+	linkReq := models.LinkRequest{
+		OriginalURL: &url,
+	}
+	_, err := c.CreateLink(linkReq)
+	assert.NotNil(t, err)
+}
+
+func TestCreateWithWrongURL(t *testing.T) {
+	c := newController()
+	url := models.URL("https://example.gr")
+	linkReq := models.LinkRequest{
+		OriginalURL: &url,
+	}
+	_, err := c.CreateLink(linkReq)
+	assert.NotNil(t, err)
+}
+
+func TestGetKownURL(t *testing.T) {
+	expectedURL := models.URL("https://example.com")
+	c := newController()
+	linkReq := models.LinkRequest{
+		OriginalURL: &expectedURL,
+	}
+	shortURL, _ := c.CreateLink(linkReq)
+	code := getCode(string(shortURL))
+	gotURL, err := c.GetLink(code)
+	assert.Nil(t, err)
+	assert.Equal(t, expectedURL, gotURL)
+}
+
+func TestGetUnkownURL(t *testing.T) {
+	c := newController()
+	_, err := c.GetLink("RandomCode")
+	assert.NotNil(t, err)
+}
+
+func getCode(url string) string {
+	return url[len(url)-testCodeSize:]
+}
